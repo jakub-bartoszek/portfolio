c5daea0d8e3bc3d46752501e6a493806a1136cce
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-23 01:31:41 +0200

    add: styled navifation item

--- app/components/navigation/navigation-item.tsx
+++ app/components/navigation/navigation-item.tsx
@@ -1,15 +1,27 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 interface NavigationItemProps {
  title: string;
  href: string;
 }
 
 export const NavigationItem = ({ title, href }: NavigationItemProps) => {
+ const pathname = usePathname();
+
  return (
-  <a
-   href={href}
-   className="font-semibold"
-  >
-   {title}
-  </a>
+  <div className="group relative flex justify-center">
+   <div />
+   <div
+    className={cn(
+     "absolute bottom-0 h-1 w-0 bg-white group-hover:w-full transition-all",
+     pathname === href && "w-full"
+    )}
+   />
+   <Link href={href}>{title}</Link>
+  </div>
  );
 };
