bfd44b776103edd3f2c72f77c81764855e56cd2d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-23 12:53:04 +0200

    change used next/image instead of img

--- app/(routes)/home/page.tsx
+++ app/(routes)/home/page.tsx
@@ -1,26 +1,34 @@
+import Image from "next/image";
+
 export default function Home() {
  return (
-  <div className="w-full h-full flex">
-   <div className="w-full h-full flex flex-col">
-    <div className="h-full flex items-center justify-start">
-     <span className="font-bold text-6xl">
-      Hello!
-      <br />I am <span className="text-[#9B9BED]">Jakub</span>
-      <br />
-      <span className="text-[#5136ed]">Web Developer</span>
-     </span>
+  <div className="w-full h-full flex flex-col">
+   <div className="w-full h-full flex flex-col md:flex-row">
+    <div className="w-full flex flex-col">
+     <div className="h-full flex items-center justify-start">
+      <span className="font-bold text-5xl md:text-7xl">
+       Hello!
+       <br />I am <span className="text-[#9B9BED]">Jakub</span>
+       <br />
+       <span className="text-[#5136ed]">Web Developer</span>
+      </span>
+     </div>
     </div>
-    <div className="flex justify-start gap-16">
-     <span>Github</span>
-     <span>LinkedIn</span>
-     <span>Facebook</span>
+    <div className="w-full h-full flex items-center justify-center">
+     <div className="relative w-full h-full">
+      <Image
+       alt="technologies"
+       src="/images/technologies-logo.png"
+       layout="fill"
+       objectFit="contain"
+      />
+     </div>
     </div>
    </div>
-   <div className="w-full h-full flex items-center justify-center">
-    <img
-     alt="technologies"
-     src="/images/technologies-logo.png"
-    />
+   <div className="flex justify-start gap-16 mt-4 md:mt-8">
+    <span>Github</span>
+    <span>LinkedIn</span>
+    <span>Facebook</span>
    </div>
   </div>
  );
