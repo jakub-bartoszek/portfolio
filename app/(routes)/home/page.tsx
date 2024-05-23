8e683d608fd05a9aa0472c4701bb64373df02d75
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-23 11:42:17 +0200

    change: changed main text

--- app/(routes)/home/page.tsx
+++ app/(routes)/home/page.tsx
@@ -2,15 +2,15 @@ export default function Home() {
  return (
   <div className="w-full h-full flex">
    <div className="w-full h-full flex flex-col">
-    <div className="h-full flex items-center justify-center">
+    <div className="h-full flex items-center justify-start">
      <span className="font-bold text-6xl">
-      Hi!
+      Hello!
+      <br />I am <span className="text-[#9B9BED]">Jakub</span>
       <br />
-      I&apos;am <span className="text-[#9B9BED]">Jakub</span> <br />
       <span className="text-[#5136ed]">Web Developer</span>
      </span>
     </div>
-    <div className="p-8 flex justify-evenly">
+    <div className="flex justify-start gap-16">
      <span>Github</span>
      <span>LinkedIn</span>
      <span>Facebook</span>
