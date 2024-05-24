5f83fcad63a6a443f861fd4f87ecb3179546806e
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-24 18:14:59 +0200

    add: added dropshadow to Image on hover

--- app/components/technologies-logo.tsx
+++ app/components/technologies-logo.tsx
@@ -24,10 +24,11 @@ const TechnologiesLogo = () => {
     />
    </div>
    <div
-    className="absolute left-[15%] top-[20%] w-[45%] aspect-square rounded-full bg-[#5136ed] shadow-[0_0_50px_0px_#5136ed70] hover:scale-105 hover:shadow-[0_0_50px_0px_#5136ed] transition duration-500"
+    className="absolute group left-[15%] top-[20%] w-[45%] aspect-square rounded-full bg-[#5136ed] shadow-[0_0_50px_0px_#5136ed70] hover:scale-105 hover:shadow-[0_0_50px_0px_#5136ed] transition duration-500"
     aria-label="Next JS Logo"
    >
     <Image
+     className="group-hover:drop-shadow-[0_0px_5px_#ffffff] transition duration-500"
      alt="Next JS"
      src="https://i.postimg.cc/J4wyJzcy/nextjs.png"
      layout="fill"
