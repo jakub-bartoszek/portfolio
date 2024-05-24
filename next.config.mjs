2ba6ed3d2c6a1e94f5312415b9691d73a9ad91c1
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-24 16:58:26 +0200

    add: added i.postimg.cc to images domaint

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- output: 'export',
+ images: {
+  domains: ['i.postimg.cc'],
+ },
 };
-module.exports = nextConfig;
+
+export default nextConfig;
