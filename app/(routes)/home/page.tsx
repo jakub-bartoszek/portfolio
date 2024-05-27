8c6fe1bfb14c1ab4226bb9fb0e56df09041fb2df
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-27 17:37:44 +0200

    change: moved footer to layout

--- app/(routes)/home/page.tsx
+++ app/(routes)/home/page.tsx
@@ -1,39 +1,18 @@
-import SocialMediaLink from "components/social-media-link";
 import TechnologiesLogo from "components/technologies-logo";
-import { prefix } from "prefix";
 
 export default function Home() {
  return (
-  <div className="w-full h-full flex flex-col">
-   <div className="w-full h-full flex flex-col md:flex-row">
-    <div className="w-full flex flex-col justify-center">
-      <span className="font-bold text-5xl md:text-7xl">
-       Hello!
-       <br />I am <span className="text-[#9B9BED]">Jakub</span>
-       <br />
-       <span className="text-[#5136ed]">Web Developer</span>
-      </span>
-    </div>
-    <div className="w-full h-full flex items-center justify-center">
-     <TechnologiesLogo />
-    </div>
+  <div className="w-full h-full flex flex-col md:flex-row">
+   <div className="w-full flex flex-col justify-center">
+    <span className="font-bold text-5xl md:text-7xl">
+     Hello!
+     <br />I am <span className="text-[#9B9BED]">Jakub</span>
+     <br />
+     <span className="text-[#5136ed]">Web Developer</span>
+    </span>
    </div>
-   <div className="flex justify-start gap-16 mt-4 md:mt-8">
-    <SocialMediaLink
-     href="https://github.com/jakub-bartoszek"
-     src={`${prefix}icons/github.svg`}
-     name="Github"
-    />
-    <SocialMediaLink
-     href="https://www.linkedin.com/in/jbartoszek-dev/"
-     src={`${prefix}icons/linkedin.svg`}
-     name="Linkedin"
-    />
-    <SocialMediaLink
-     href="https://www.facebook.com/profile.php?id=61554246347111"
-     src={`${prefix}icons/facebook.svg`}
-     name="Facebook"
-    />
+   <div className="w-full h-full flex items-center justify-center">
+    <TechnologiesLogo />
    </div>
   </div>
  );
--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "components/footer/footer";
 import { Navigation } from "components/navigation/navigation";
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
@@ -5,6 +6,7 @@ const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   <div className="flex flex-col max-w-[1400px] w-[1400px] min-h-dvh">
    <Navigation />
    <main className="h-full p-6">{children}</main>
+   <Footer />
   </div>
  );
 };
