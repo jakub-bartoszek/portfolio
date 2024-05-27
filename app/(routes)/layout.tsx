c07e7c9cbb583c4392c613edead2366bd50ed491
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-27 17:17:41 +0200

    add: added min height to main layout

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -2,7 +2,7 @@ import { Navigation } from "components/navigation/navigation";
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
  return (
-  <div className="flex flex-col h-full max-w-[1400px] w-[1400px]">
+  <div className="flex flex-col max-w-[1400px] w-[1400px] min-h-dvh">
    <Navigation />
    <main className="h-full p-6">{children}</main>
   </div>
