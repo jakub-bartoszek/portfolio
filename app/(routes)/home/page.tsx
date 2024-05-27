c1fa5bb8452f4d48629dd1ff2c20cd214ee1e3eb
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-27 17:20:48 +0200

    delete: removed not necessary div

--- app/(routes)/home/page.tsx
+++ app/(routes)/home/page.tsx
@@ -6,15 +6,13 @@ export default function Home() {
  return (
   <div className="w-full h-full flex flex-col">
    <div className="w-full h-full flex flex-col md:flex-row">
-    <div className="w-full flex flex-col">
-     <div className="h-full flex items-center justify-start">
+    <div className="w-full flex flex-col justify-center">
       <span className="font-bold text-5xl md:text-7xl">
        Hello!
        <br />I am <span className="text-[#9B9BED]">Jakub</span>
        <br />
        <span className="text-[#5136ed]">Web Developer</span>
       </span>
-     </div>
     </div>
     <div className="w-full h-full flex items-center justify-center">
      <TechnologiesLogo />
