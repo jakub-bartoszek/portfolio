483a86247490089adea00a6110ce879169edf9c4
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-29 15:09:53 +0200

    change: changed div to h1

--- app/(routes)/home/page.tsx
+++ app/(routes)/home/page.tsx
@@ -3,14 +3,14 @@ import TechnologiesLogo from "components/technologies-logo";
 export default function Home() {
  return (
   <div className="w-full h-full flex flex-col md:flex-row">
-   <div className="w-full flex flex-col justify-center">
+   <h1 className="w-full flex flex-col justify-center">
     <span className="font-bold text-5xl md:text-7xl">
      Hello!
      <br />I am <span className="text-[#9B9BED]">Jakub</span>
      <br />
      <span className="text-[#5136ed]">Web Developer</span>
     </span>
-   </div>
+   </h1>
    <div className="w-full h-full flex items-center justify-center">
     <TechnologiesLogo />
    </div>
