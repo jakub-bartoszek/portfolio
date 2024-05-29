6cdba4ad3627bc80dc4c53a2a677e0d4968e6d62
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-29 13:53:45 +0200

    change: changed display of links

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,22 +3,24 @@ import { prefix } from "prefix";
 
 export const Footer = () => {
  return (
-  <footer className="flex justify-center md:justify-start gap-16 p-6">
-   <SocialMediaLink
-    href="https://github.com/jakub-bartoszek"
-    src={`${prefix}icons/github.svg`}
-    name="Github"
-   />
-   <SocialMediaLink
-    href="https://www.linkedin.com/in/jbartoszek-dev/"
-    src={`${prefix}icons/linkedin.svg`}
-    name="Linkedin"
-   />
-   <SocialMediaLink
-    href="https://www.facebook.com/profile.php?id=61554246347111"
-    src={`${prefix}icons/facebook.svg`}
-    name="Facebook"
-   />
+  <footer className="w-full">
+   <div className="flex w-full justify-around p-6 md:pr-0 md:w-1/2 md:justify-between">
+    <SocialMediaLink
+     href="https://github.com/jakub-bartoszek"
+     src={`${prefix}icons/github.svg`}
+     name="Github"
+    />
+    <SocialMediaLink
+     href="https://www.linkedin.com/in/jbartoszek-dev/"
+     src={`${prefix}icons/linkedin.svg`}
+     name="Linkedin"
+    />
+    <SocialMediaLink
+     href="https://www.facebook.com/profile.php?id=61554246347111"
+     src={`${prefix}icons/facebook.svg`}
+     name="Facebook"
+    />
+   </div>
   </footer>
  );
 };
