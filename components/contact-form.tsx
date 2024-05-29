d4014838a812c566c9b03938299c7e0004ebd58d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-29 11:49:28 +0200

    add: added console log of emailJs variables

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -20,6 +20,8 @@ const ContactForm = ({
  const [message, setMessage] = useState("");
  const [formStatus, setFormStatus] = useState("");
 
+ console.log(serviceKey, templateKey, userKey)
+
  const onFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
   e.preventDefault();
   setFormStatus("loading");
