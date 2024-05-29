d2266a89a4e7a7a0389e63280924809d7fca6d5d
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-29 15:10:24 +0200

    change: changed border and background of tile

--- components/repository-tile.tsx
+++ components/repository-tile.tsx
@@ -3,28 +3,30 @@ import Link from "next/link";
 
 export const RepositoryTile = ({ repository }: { repository: Repository }) => {
  return (
-  <div className="p-6 border-2 border-[#5136ed] rounded-xl flex flex-col gap-4">
+  <div className="p-6 bg-[#5136ed]/10 hover:bg-[#5136ed]/20 rounded-xl flex flex-col gap-4 transition">
    <h2 className=" font-bold text-2xl">{repository.name}</h2>
-   <span>{repository.description}</span>
-   <div>
-    <p>
-     <b>Repository: </b>
-     <Link
-      className="text-[#9b9bed]"
-      href={repository.html_url}
-     >
-      {repository.html_url}
-     </Link>
-    </p>
-    <p>
-     <b>Demo: </b>
-     <Link
-      className="text-[#9b9bed]"
-      href={repository.homepage}
-     >
-      {repository.homepage}
-     </Link>
-    </p>
+   <div className="flex flex-col justify-between h-full gap-4">
+    <span>{repository.description}</span>
+    <div>
+     <p>
+      <b>Repository: </b>
+      <Link
+       className="text-[#9b9bed]"
+       href={repository.html_url}
+      >
+       {repository.html_url}
+      </Link>
+     </p>
+     <p>
+      <b>Demo: </b>
+      <Link
+       className="text-[#9b9bed]"
+       href={repository.homepage}
+      >
+       {repository.homepage}
+      </Link>
+     </p>
+    </div>
    </div>
   </div>
  );
