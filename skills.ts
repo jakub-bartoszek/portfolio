a0ca0db4d9faec954615c2697db88f5e66be38c9
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-29 16:56:22 +0200

    add: added skill.ts

---
+++ skills.ts
@@ -0,0 +1,60 @@
+export const skills = [
+ {
+  section: "Frontend Technologies",
+  skills: [
+   "React.js",
+   "Next.js",
+   "JavaScript ESNext",
+   "TypeScript",
+   "React Hooks",
+   "React Router",
+   "Styled components",
+   "Tailwind CSS",
+   "SASS",
+   "CSS BEM convention",
+   "CSS Grid",
+   "CSS Flexbox"
+  ]
+ },
+ {
+  section: "State Management",
+  skills: ["Redux-Saga", "Redux (Toolkit)", "Signal"]
+ },
+ {
+  section: "Backend Technologies",
+  skills: ["Express.js", "Node.js"]
+ },
+ {
+  section: "Data Management",
+  skills: [
+   "Working with API (fetch, axios)",
+   "Promises, Async/Await",
+   "Error handling",
+   "Database management",
+   "MySQL",
+   "Tanstack Query",
+   "Immutability"
+  ]
+ },
+ {
+  section: "Testing & Quality Assurance",
+  skills: ["Cypress", "Jest", "Debounce/Throttle"]
+ },
+ {
+  section: "Version Control & Project Management",
+  skills: ["Git", "GitHub Pull Requests & Review", "Trello", "Scrum", "NPM"]
+ },
+ {
+  section: "Design Tools",
+  skills: ["Adobe Photoshop", "Adobe Illustrator", "Adobe InDesign"]
+ },
+ {
+  section: "Other Skills",
+  skills: [
+   "Teamwork",
+   "Markdown",
+   "Semantic & accessible HTML",
+   "Responsive Web Design"
+  ]
+ }
+];
