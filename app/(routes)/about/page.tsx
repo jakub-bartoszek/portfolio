b041c19f7f5b1449a32d1d2c9af14804bc80ffb6
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-05-29 14:27:55 +0200

    change: changed span to h1 and changed gap

--- app/(routes)/about/page.tsx
+++ app/(routes)/about/page.tsx
@@ -4,12 +4,10 @@ import Link from "next/link";
 export default function About() {
  return (
   <div className="w-full h-full flex flex-col md:flex-row">
-   <div className="w-full flex flex-col justify-center gap-16">
-    <span className="font-bold text-5xl md:text-7xl text-[#9b9bed]">
-     About me
-    </span>
-    <div className="flex flex-col gap-4 text-2xl">
-     <p className="text-lg md:text-xl">
+   <div className="w-full flex flex-col gap-8">
+    <h1 className="font-bold text-5xl md:text-7xl text-[#9b9bed]">About me</h1>
+    <div className="flex flex-col gap-4 text-lg md:text-xl">
+     <p>
       I am
       <span className="text-[#9B9BED]">&nbsp;Jakub Bartoszek</span>, a young web
       developer with a focus on frontend development, although I consider myself
@@ -33,7 +31,7 @@ export default function About() {
       </Link>
       .
      </p>
-     <p className="text-lg md:text-xl mt-4">
+     <p>
       I finished IT technician school and hold the
       <span className="text-[#c3a0ff]">&nbsp;INF.02&nbsp;</span>
       and
@@ -51,10 +49,10 @@ export default function About() {
       working with others, enjoying the sharing of ideas and learning new things
       from my peers.
      </p>
-     <p className="text-lg md:text-xl mt-4">
+     <p>
       When I am not programming, I spend time playing guitar, going to the gym,
-      making music, reading books, creating graphic designs, and working on my car. I also
-      enjoy spending time outdoors and love going for walks.
+      making music, reading books, creating graphic designs, and working on my
+      car. I also enjoy spending time outdoors and love going for walks.
      </p>
     </div>
    </div>
--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -1,9 +1,9 @@
 import { prefix } from "prefix";
 import { Mail, Phone } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
 import TechnologiesLogo from "components/technologies-logo";
-import ContactForm from "components/contact-form";
+import ContactForm from "components/contact/contact-form";
+import ContactItem from "components/contact/contact-item";
 
 export default function Contact() {
  const serviceKey = process.env.EMAIL_JS_SERVICE;
@@ -12,62 +12,51 @@ export default function Contact() {
 
  return (
   <div className="w-full h-full flex flex-col md:flex-row">
-   <div className="w-full flex flex-col justify-around gap-16">
-    <div>
-     <span className="font-bold text-5xl md:text-7xl text-[#9b9bed]">
-      Contact Me
-     </span>
-     <p className="text-lg md:text-xl">
+   <div className="flex flex-col gap-8">
+    <h1 className="font-bold text-5xl md:text-7xl text-[#9b9bed]">
+     Contact Me
+    </h1>
+    <div className="flex flex-col gap-4 text-lg">
+     <p>
       I am always open to discussing web development work or partnership
       opportunities. Feel free to reach out to me through any of the following
       contact options:
      </p>
-     <ul className="text-lg md:text-xl mt-4">
-      <li className="flex items-center gap-4">
-       <Phone className="h-6 w-6" />
-       <span className="text-[#9B9BED]">+48 530 936 096</span>
-      </li>
-      <li className="flex items-center gap-4">
-       <Mail />
-       <Link
-        className="text-[#9B9BED]"
-        href="mailto:jakub.bartoszek.dev@gmail.com"
-       >
-        jakub.bartoszek.dev@gmail.com
-       </Link>
-      </li>
-      <li className="flex items-center gap-4">
-       <Image
-        src={`${prefix}icons/linkedin.svg`}
-        alt="Linkedin"
-        width={24}
-        height={24}
-       />
-       <Link
-        className="text-[#9B9BED]"
-        href="https://www.linkedin.com/in/jakub-bartoszek"
-        target="_blank"
-        rel="noopener noreferrer"
-       >
-        linkedin.com/in/jakub-bartoszek
-       </Link>
-      </li>
-      <li className="flex items-center gap-4">
-       <Image
-        src={`${prefix}icons/github.svg`}
-        alt="Github"
-        width={24}
-        height={24}
-       />
-       <Link
-        className="text-[#9B9BED]"
-        href="https://www.github.com/jakub-bartoszek"
-        target="_blank"
-        rel="noopener noreferrer"
-       >
-        linkedin.com/in/jakub-bartoszek
-       </Link>
-      </li>
+     <ul>
+      <ContactItem
+       icon={<Phone className="h-6 w-6" />}
+       text="+48 530 936 096"
+       href="tel:530-936-096"
+      />
+      <ContactItem
+       icon={<Mail />}
+       text="jakub.bartoszek.dev@gmail.com"
+       href="mailto:jakub.bartoszek.dev@gmail.com"
+      />
+      <ContactItem
+       icon={
+        <Image
+         src={`${prefix}icons/linkedin.svg`}
+         alt="Linkedin"
+         width={24}
+         height={24}
+        />
+       }
+       text="linkedin.com/in/jakub-bartoszek"
+       href="https://www.linkedin.com/in/jakub-bartoszek"
+      />
+      <ContactItem
+       icon={
+        <Image
+         src={`${prefix}icons/github.svg`}
+         alt="Github"
+         width={24}
+         height={24}
+        />
+       }
+       text="github.com/jakub-bartoszek"
+       href="https://www.github.com/jakub-bartoszek"
+      />
      </ul>
     </div>
     <ContactForm
