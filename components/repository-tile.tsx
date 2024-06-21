e8b7024205dfba18bc161a4c84814a718bb30c56
Jakub Bartoszek <k.bartoszek507@wp.pl>
2024-06-21 14:53:45 +0200

    add: added truncate and hover effect to links

--- components/repository-tile.tsx
+++ components/repository-tile.tsx
@@ -8,34 +8,30 @@ export const RepositoryTile = ({
 }) => {
  return (
   <div className="p-6 bg-[#5136ed]/10 hover:bg-[#5136ed]/20 rounded-xl flex flex-col gap-4 transition">
-   <h2 className=" font-bold text-2xl">{repository.name}</h2>
-   <div className="flex flex-col justify-between h-full gap-4">
-    <span>{repository.description}</span>
-    <div>
+    <h2 className="font-bold text-2xl">{repository.name}</h2>
+   <section className="flex flex-col justify-between h-full gap-4">
+    <p>{repository.description}</p>
+    <div className="flex flex-col gap-2">
      {repository.html_url && (
-      <p>
-       <b>Repository: </b>
-       <Link
-        className="text-[#9b9bed]"
-        href={repository.html_url || ""}
-       >
-        {repository.html_url}
-       </Link>
-      </p>
+      <Link
+       className="text-[#9b9bed] block w-full truncate hover:text-[#b7b7f8] transition"
+       href={repository.html_url || ""}
+      >
+       <strong className="text-white">Repository: </strong>
+       {repository.html_url}
+      </Link>
      )}
      {repository.homepage && (
-      <p>
-       <b>Demo: </b>
-       <Link
-        className="text-[#9b9bed]"
-        href={repository.homepage || ""}
-       >
-        {repository.homepage}
-       </Link>
-      </p>
+      <Link
+       className="text-[#9b9bed] block w-full truncate hover:text-[#b7b7f8] transition"
+       href={repository.homepage || ""}
+      >
+       <strong className="text-white">Demo: </strong>
+       {repository.homepage}
+      </Link>
      )}
     </div>
-   </div>
+   </section>
   </div>
  );
 };
